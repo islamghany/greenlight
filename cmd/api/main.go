59366850d77159362e7eafbfbbfdcad6845f9ded
islamghany <islamghany3@gmail.com>
2021-12-26 13:11:59 +0200

    connect to postgresSQL

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 // version of the application
@@ -16,6 +20,9 @@ const version = "1.0.0"
 type config struct {
 	port int    // port number e.g. 8080
 	env  string // development ot prodction
+	db   struct {
+		dsn string
+	}
 }
 
 // app struct to hold the http handlers, helpers and middleware
@@ -29,19 +36,27 @@ func main() {
 
 	flag.IntVar(&conf.port, "port", 4000, "API Server port")
 	flag.StringVar(&conf.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&conf.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "data source name")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(conf)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	// Defer a call to db.Close() so that the connection pool is closed before the
+	// main() function exits.
+	defer db.Close()
+
+	logger.Printf("database connection pool established")
 
 	app := &application{
 		config: conf,
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/health", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello from the other side\n")
-	})
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", conf.port),
 		Handler:      app.routes(),
@@ -51,8 +66,32 @@ func main() {
 	}
 	logger.Printf("Server is running on port %d in %s mode", app.config.port, conf.env)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		logger.Fatal(err)
 	}
+}
+
+func openDB(conf config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", conf.db.dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a context with a 5-second timeout deadline.
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Use PingContext() to establish a new connection to the database, passing in the
+	// context we created above as a parameter. If the connection couldn't be
+	// established successfully within the 5 second deadline, then this will return an
+	// error.
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return the sql.DB connection pool.
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module islamghany.greenlight
 
 go 1.17
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require (
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/lib/pq v1.10.0 // indirect
+)
