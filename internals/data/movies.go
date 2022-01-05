a7c229471a2b6f65ec32b815a2fe36b8b391be77
islamghany <islamghany3@gmail.com>
2022-01-05 14:14:28 +0200

    filtering the query

--- internals/data/movies.go
+++ internals/data/movies.go
@@ -44,14 +44,16 @@ type MovieModel struct {
 
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
 	query := `
-		SELECT id, created_at, title, year, runtime, genres, version
-		FROM movies
-		ORDER BY id
-	`
+        SELECT id, created_at, title, year, runtime, genres, version
+        FROM movies
+        WHERE (LOWER(title) = LOWER($1) OR $1 = '') 
+        AND (genres @> $2 OR $2 = '{}')     
+        ORDER BY id`
+
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	rows, err := m.DB.QueryContext(ctx, query)
+	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres))
 	if err != nil {
 		return nil, err
 	}
