227e5a2fdc19afcb08ef88358f52db25a2f5b292
islamghany <islamghany3@gmail.com>
2022-01-05 07:05:56 +0200

    add optimistic concurrency controll to update movie

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -148,7 +148,12 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 	}
 	err = app.models.Movies.Update(movie)
 	if err != nil {
-		app.serverErrorResponse(w, r, err)
+		switch {
+		case errors.Is(err, data.ErrEditConflict):
+			app.editConflictResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
 		return
 	}
 
--- internals/data/models.go
+++ internals/data/models.go
@@ -7,6 +7,7 @@ import (
 
 var (
 	ErrRecordNotFound = errors.New("record not found")
+	ErrEditConflict   = errors.New("edit conflict")
 )
 
 type Models struct {
