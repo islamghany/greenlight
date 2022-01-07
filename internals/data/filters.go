377bfb90175ad0a4f8da92f76442d742afe26de4
islamghany <islamghany3@gmail.com>
2022-01-07 19:10:03 +0200

    add pagination to the movies

--- internals/data/filters.go
+++ internals/data/filters.go
@@ -35,3 +35,10 @@ func (f *Filters) sortDirection() string {
 	}
 	return "ASC"
 }
+
+func (f *Filters) limit() int {
+	return f.PageSize
+}
+func (f *Filters) offset() int {
+	return (f.Page - 1) * f.PageSize
+}
