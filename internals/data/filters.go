27f09ee9b48be23ccba50c81bbfc2e21f5cb8e3c
islamghany <islamghany3@gmail.com>
2022-01-07 19:01:38 +0200

    add sort by to the get all query

--- internals/data/filters.go
+++ internals/data/filters.go
@@ -1,6 +1,8 @@
 package data
 
 import (
+	"strings"
+
 	"islamghany.greenlight/internals/validator"
 )
 
@@ -17,4 +19,19 @@ func (f *Filters) ValidateFilters(v *validator.Validator) {
 	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 	v.Check(validator.In(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
+}
+
+func (f *Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+	panic("unsafe sort provided: " + f.Sort)
+}
+func (f *Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
 }
--- internals/data/movies.go
+++ internals/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/lib/pq"
@@ -43,13 +44,13 @@ type MovieModel struct {
 }
 
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
-	// Use full-text search for the title filter.
-	query := `
-        SELECT id, created_at, title, year, runtime, genres, version
-        FROM movies
-		WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '') 
-        AND (genres @> $2 OR $2 = '{}')     
-        ORDER BY id`
+
+	query := fmt.Sprintf(`
+	SELECT id, created_at, title, year, runtime, genres, version
+	FROM movies
+	WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '') 
+	AND (genres @> $2 OR $2 = '{}')     
+	ORDER BY %s %s, id ASC`, filters.sortColumn(), filters.sortDirection())
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
