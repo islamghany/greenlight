83c2933ed886d25f233ab0093fdb8bb61681649c
islamghany <islamghany3@gmail.com>
2022-01-18 07:33:28 +0200

    intercepring the shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,9 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"fmt"
-	"log"
-	"net/http"
 	"os"
 	"time"
 
@@ -77,24 +74,7 @@ func main() {
 		models: data.NewModels(db),
 	}
 
-	srv := &http.Server{
-		Addr:        fmt.Sprintf(":%d", conf.port),
-		Handler:     app.routes(),
-		IdleTimeout: time.Minute,
-		// Create a new Go log.Logger instance with the log.New() function, passing in
-		// our custom Logger as the first parameter. The "" and 0 indicate that the
-		// log.Logger instance should not use a prefix or any flags.
-		ErrorLog:     log.New(logger, "", 0),
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
-	}
-
-	logger.PrintInfo("starting server", map[string]string{
-		"addr": srv.Addr,
-		"env":  conf.env,
-	})
-
-	err = srv.ListenAndServe()
+	err = app.serve()
 	if err != nil {
 		logger.PrintFatal(err, nil)
 	}
---
+++ cmd/api/server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func (app *application) serve() error {
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", app.config.port),
+		Handler:      app.routes(),
+		ErrorLog:     log.New(app.logger, "", 0),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	// intercepting the shutdown
+	go func() {
+		quit := make(chan os.Signal, 1)
+
+		// Use signal.Notify() to listen for incoming SIGINT and SIGTERM signals and
+		// relay them to the quit channel. Any other signals will not be caught by
+		// signal.Notify() and will retain their default behavior.
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		// Read the signal from the quit channel. This code will block until a signal is
+		// received.
+		s := <-quit
+
+		// Log a message to say that the signal has been caught. Notice that we also
+		// call the String() method on the signal to get the signal name and include it
+		// in the log entry properties.
+		app.logger.PrintInfo("caught signal", map[string]string{
+			"signal": s.String(),
+		})
+
+		// Exit the application with a 0 (success) status code.
+		os.Exit(0)
+	}()
+	app.logger.PrintInfo("Starting Server", map[string]string{
+		"addr": srv.Addr,
+		"env":  app.config.env,
+	})
+	return srv.ListenAndServe()
+}
