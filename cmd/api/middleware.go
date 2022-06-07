6dd57c5e332c54c4b1a1a556163c036599372340
islamghany <islamghany3@gmail.com>
2022-06-07 18:08:53 +0200

    added the activeated property to the openapi file

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -295,8 +295,8 @@ func (app *application) requirePermission(code string, next http.HandlerFunc) ht
 
 func (app *application) enableCORs(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		//	origin := r.Header.Get("Origin")
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		origin := r.Header.Get("Origin")
+		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PATCH")
 		w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
