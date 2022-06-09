930ac0439943472c5388408db49c69d6d0cba9fd
islamghany <islamghany3@gmail.com>
2022-06-09 13:21:05 +0200

    added the unique of the userid with movieid in likes

--- migrations/000008_create_likes_table.up.sql
+++ migrations/000008_create_likes_table.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS likes (
     id bigserial PRIMARY KEY,
     movie_id bigint NOT NULL REFERENCES movies ON DELETE CASCADE,
-    user_id bigint NOT NULL REFERENCES users ON DELETE CASCADE
+    user_id bigint NOT NULL REFERENCES users ON DELETE CASCADE,
+    UNIQUE(movie_id, user_id)
 );
