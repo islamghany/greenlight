00877c3ddfc6366d29293e39898bbc45c392093b
islamghany <islamghany3@gmail.com>
2022-12-05 11:53:23 +0200

    finshied the activated user handler after connected the auth service with message broker

--- auth-service/api/routes.go
+++ auth-service/api/routes.go
@@ -16,6 +16,7 @@ func (server *Server) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/accounts/users", server.registerUserHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/accounts/users/:id", server.getUserHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/accounts/users/logout", server.authenticateMiddleware(server.logoutHandler))
+	router.HandlerFunc(http.MethodPatch, "/v1/accounts/users/activate", server.activateUserHandler)
 
 	router.HandlerFunc(http.MethodPost, "/v1/accounts/tokens/authentication", server.createAuthenticationTokenHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/accouts/tokens/renew-access-token", server.renewAccessTokenHandler)
